a0661e7e66a865e2b1ed88f232e1c4f9938978a2
Igor-A-Penkin <penkin.igor@gmail.com>
2021-02-19 01:32:13 +0300

    Empty project

---
+++ Questionnaire/Questionnaire/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Questionnaire
+//
+//  Created by Игорь Пенкин on 18.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
